8ff34aa378250aa2c76697c2529b896b6dd99d09
Sodment <sodment@gmail.com>
2021-04-15 12:25:30 +0200

    Changed acces modifiers of Node.cs and added Struct for cube coordinates

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    //Cube coordinates
-    int x;
-    int y;
-    int z;
+
+    public readonly struct CubeCoordinates
+    {
+
+        public CubeCoordinates(int X, int Y, int Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+        public int x { get; }
+        public int y { get; }
+        public int z { get; }
+        
+    };
+
+    public bool isOccupied = false;
+    public CubeCoordinates cubeCoordinates;
 
     private void Start()
     {
-        x = Mathf.FloorToInt(transform.position.x / 0.866f);
-        z = Mathf.FloorToInt(transform.position.z) - (x - (x & 1)) / 2;
-        y = -x - z;
+        Vector3 position = transform.position;
+        int x = Mathf.FloorToInt(position.x / 0.866f);
+        int z = Mathf.FloorToInt(position.z) - (x - (x & 1)) / 2;
+        int y = -x - z;
+        cubeCoordinates = new CubeCoordinates(x, y, z);
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("X: " + x + " Y: " + y + "Z: " + z);
+        Debug.Log("X: " + cubeCoordinates.x + " Y: " + cubeCoordinates.y + "Z: " + cubeCoordinates.z);
     }
 
     private void OnMouseEnter()
@@ -29,3 +45,5 @@ public class Node : MonoBehaviour
         GetComponent<Renderer>().material.color /= 1.2f;
     }
 }
+
+
