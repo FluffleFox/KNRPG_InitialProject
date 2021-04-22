135116beea243fad3bf306dbc268fd53ffce5544
Sodment <sodment@gmail.com>
2021-04-22 15:04:17 +0200

    Added script for playing sound in given stage

---
+++ Assets/Scripts/MusicScripts/PlaySoundOnStage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+    public class PlaySoundOnStage : MonoBehaviour
+    {
+        public GameStateMachine.GameState state;
+        public AudioSource source;
+        private float delay = 0.5f;
+
+        private void Start()
+        {
+            switch (state)
+            {
+                case GameStateMachine.GameState.FreeMove:
+                {
+                    break;
+                }
+                case GameStateMachine.GameState.Planning:
+                {
+                    break;
+                }
+                case GameStateMachine.GameState.Fighting:
+                {
+                    break;
+                }
+                case GameStateMachine.GameState.Summary:
+                {
+                    break; 
+                }
+            }
+        }
+
+        private void PlaySound()
+        {
+            if(source.gameObject.activeSelf){source.PlayDelayed(delay);}
+        }
+
+        private void StopSound()
+        {
+            if(source.gameObject.activeSelf){source.Stop();}
+        }
+    }
